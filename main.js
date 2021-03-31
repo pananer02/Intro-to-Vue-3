558562b6ae57838886647f00543b24a119e68336
pananer02 <pananer02@hotmail.com>
2021-03-31 14:38:31 +0700

    as

--- main.js
+++ main.js
@@ -11,5 +11,11 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    method: {
+        addToCart(){
+            console.log('test');    
+            this.cart += 1;
+        }
     }
 })
