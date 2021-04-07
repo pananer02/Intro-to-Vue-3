580ecd1f9db9664b531e464d5d0510dfb2f846ce
pananer02 <pananer02@hotmail.com>
2021-04-07 17:01:30 +0700

    asd

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,35 +1,47 @@
 app.component('product-display', {
-    template: 
-      /*html*/ 
-      `<div class="product-display">
-        <div class="product-container">
-          <div class="product-image">
-            <img v-bind:src="image">
-          </div>
-          <div class="product-info">
-            <h1>{{ title }}</h1>
-    
-            <p v-if="inStock">In Stock</p>
-            <p v-else>Out of Stock</p>
-    
-            <div 
-              v-for="(variant, index) in variants" 
-              :key="variant.id" 
-              @mouseover="updateVariant(index)" 
-              class="color-circle" 
-              :style="{ backgroundColor: variant.color }">
-            </div>
-            
-            <button 
-              class="button" 
-              :class="{ disabledButton: !inStock }" 
-              :disabled="!inStock" 
-              v-on:click="addToCart">
-              Add to Cart
-            </button>
-          </div>
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: 
+  /*html*/
+  `<div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img v-bind:src="image">
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+
+        <p>Shipping: {{ shipping }}</p>
+        
+        <!-- solution -->
+        <product-details :details="details"></product-details>
+        <!-- solution -->
+
+        <div 
+          v-for="(variant, index) in variants" 
+          :key="variant.id" 
+          @mouseover="updateVariant(index)" 
+          class="color-circle" 
+          :style="{ backgroundColor: variant.color }">
         </div>
-      </div>`,
+        
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="addToCart">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  </div>`,
   data() {
     return {
         product: 'Socks',
@@ -59,6 +71,12 @@ app.component('product-display', {
       },
       inStock() {
           return this.variants[this.selectedVariant].quantity
+      },
+      shipping() {
+        if (this.premium) {
+          return 'Free'
+        }
+        return 2.99
       }
   }
 })
